3a4a000b4a87e664805ea048f745cb3cf95d2d45
github username <github email address>
2019-06-18 10:06:50 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  User.create!(name: "Malcome", password: "password", data: 100)
+
+5.times do
+  Service.create!(
+    name: Faker::TvShows::FamilyGuy.character,
+    description: Faker::Quotes::Shakespeare.hamlet_quote,
+    price: rand(1...5)
+  )
+end
+5.times do
+  Ai.create!(
+    name: Faker::Movies::HarryPotter.character,
+    user_id: 1,
+    description: Faker::Movies::StarWars.wookiee_sentence,
+    balance: rand(1...5)
+  )
+end
+5.times do
+  Transaction.create!(
+    status: nil,
+    service_id: 1,
+    ai_id: 1
+  )
+end
