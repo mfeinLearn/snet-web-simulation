f8ffd81eda80581dfc818581cc1341aa1e8e9a12
github username <github email address>
2019-06-19 18:33:37 -0400

    add fix to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'sessions#home'
 get '/signup' => 'users#new'
+post '/signup' => 'users#create'
 # get '/login' => 'sessions#new'
 # post 'login' => 'sessions#create'
-get '/signin' => 'sessions#new'
-post '/signin' => 'sessions#create'
+get '/login' => 'sessions#new'
+post '/login' => 'sessions#create'
 delete '/logout' => 'sessions#destroy'
 resources :users
 resources :ais#, only:[:new ,:create, :show, :index, :update, :edit]
