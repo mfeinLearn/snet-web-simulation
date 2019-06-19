45ada4f4a6eb9f16d28f1cdcb2b8b6d6c0826b84
github username <github email address>
2019-06-19 18:27:42 -0400

    add validations

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,5 +2,9 @@ class Service < ApplicationRecord
 has_many :transactions
 has_many :ais, through: :transactions
 has_many :users, through: :ais
+
+ validates :name, presence: true
+ validates :description, presence: true
+ validates :price, presence: true
 end
 #user -< ai -<Transaction >- service
