fbf0fe9db067abff55e0fe32f64207834a59ec4d
github username <github email address>
2019-06-19 18:27:03 -0400

    add scope and class method

--- app/models/ai.rb
+++ app/models/ai.rb
@@ -6,4 +6,11 @@ has_many :services, through: :transactions
 validates :name, :description, presence: true
 
 accepts_nested_attributes_for :services
+
+  scope :order_by_ais, -> {order(balance: :desc)}
+
+  def name_and_balance # a reader method to display flavor and brand
+  "Name: #{name} - Balance: #{balance}"
+  end
+
 end
