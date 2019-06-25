5cc98e9fe05141cc537bd3658e1e3dfcc27a9438
github username <github email address>
2019-06-25 06:23:34 -0400

    add use method

--- app/models/service.rb
+++ app/models/service.rb
@@ -7,6 +7,15 @@ has_many :users, through: :ais
  validates :description, presence: true
  validates :price, presence: true
 
+def use
+  if self.users[0].data - self.price < self.users[0].data
+    return "please get more data!"
+  else
+    result = self.users[0].data - self.price
+    return "this is the result #{result}!"
+  #return "The service is in use"
+  end
+end
 
 end
 #user -< ai -<Transaction >- service
