f1dd2b51f7c6833733ee696aa0c5d54b8f27ff86
github username <github email address>
2019-06-25 10:47:49 -0400

    add new and create action to transaction controller and create a new transaction and render it to the transaction show view

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,12 +1,28 @@
 class TransactionsController < ApplicationController
+  def new
+    @transaction =Transaction.new
+  end
+
+  def create
+
+    @transaction = Transaction.create(
+      service_id: params[:transaction][:service_id],
+      status: params[:transaction][:status],
+      ai_id: params[:transaction][:ai_id])
+      #raise @transaction.inspect
+    redirect_to transaction_path(@transaction)
+  end
 
   def show
     @transaction = Transaction.find_by(id: params[:id])
-
+    #byebug
+    #@ai = Ai.find_by(id: params[:id])
+    #@service = Service.find_by(id: params[:id])
+#byebug
   end
 
   def update
-
+#byebug
     @transaction = Transaction.find_by(id: params[:id])
     if @transaction.status == nil
       @transaction.done_transaction
@@ -17,6 +33,8 @@ class TransactionsController < ApplicationController
       redirect_to transaction_path(@transaction)
     end
   end
+
+
 
 
 end
