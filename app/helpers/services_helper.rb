7ce32e6ae266e79edd9960c6690f5675d634ae83
github username <github email address>
2019-06-26 15:16:28 -0400

    will add in a bit - streach goal

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -3,4 +3,7 @@ module ServicesHelper
   #  service.ai.nil? ? link_to("Add Ai", edit_service_path(service)) : link_to(service.ai_name, ai_path(service.ai))
     service.ai.nil? ? link_to("Add Ai") : link_to(service.name)
   end
+  def use(user,service)
+
+  end
 end
