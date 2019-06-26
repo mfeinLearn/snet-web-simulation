c48145b847e2576f7fe441a89086ad5dc166de8d
github username <github email address>
2019-06-26 14:40:27 -0400

    add attribute writer for service

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :ai
   belongs_to :service
 #  belongs_to :user, through: :ai
-
+accepts_nested_attributes_for :service
   def done_transaction
     # self.status
     # sleep(1.minutes)
