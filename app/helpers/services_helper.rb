0fdd569e0a1b92d6a6f3d8d9db9bd6b475ad53ed
github username <github email address>
2019-06-27 12:26:19 -0400

    add helper to check to see if user is current user

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,9 +1,16 @@
 module ServicesHelper
   def display_ai(service)
   #  service.ai.nil? ? link_to("Add Ai", edit_service_path(service)) : link_to(service.ai_name, ai_path(service.ai))
-    service.ai.nil? ? link_to("Add Ai") : link_to(service.name)
+  #  service.ai.nil? ? link_to("Add Ai") : link_to(service.name)
   end
-  def use(user,service)
+  # def use(user,service)
+  #
+  # end
 
+  def if_current_user_has_service(ai)
+    if ai.user_id == current_user.id
+      content_tag(:p, "The folloiwng are the AIs that you created for this service: ")
+      content_tag(:p, "Name of Ai:")
+    end
   end
 end
