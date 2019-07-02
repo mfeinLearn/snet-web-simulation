7649d98cce01360e23b69c8550612777c157d49d
github username <github email address>
2019-07-02 14:21:35 -0400

    add comments and call search method on Ai collection

--- app/controllers/ais_controller.rb
+++ app/controllers/ais_controller.rb
@@ -1,8 +1,11 @@
 class AisController < ApplicationController
 
 def new
+  # create a new instance of Ai
   @ai = Ai.new
+  # built a new transaction on the ai which will associate the transaction to the ai
   @transaction = @ai.transactions.build
+  # the service is associated with the transaction
   @transaction.build_service
 
 #   @ai = Ai.new(user_id: @user.id)
@@ -10,16 +13,18 @@ def new
 
 end
 
+# saves a new ai
 def create
   @ai = current_user.ais.build(ai_params)
-  if current_user.data > 220
-    if params[:ai][:transactions_attributes]["0"][:service_id] != ""
+  if current_user.data > 220 # check if the current user date is greater then 220
+    if params[:ai][:transactions_attributes]["0"][:service_id] != "" # if selected in the dropdown
          @ai.save
          @transaction = @ai.transactions.last
          @transaction.service_id = params[:ai][:transactions_attributes]["0"][:service_id]
          @transaction.save
         redirect_to edit_transaction_path(@transaction)
     else
+      # if typed in manually
       if @ai.save!
         redirect_to new_transaction_path
       else
@@ -32,23 +37,26 @@ def create
 end
 
 
-
+  # view all of the ais
 def index
-  @ais = Ai.order_by_price
+  @ais = Ai.search(params[:search])
+  # @ais = Ai.order_by_price
   #@ais = Ai.order_by_ais
 end
 
-
+  # show one ai
 def show
   set_ai
   @services = @ai.services
 end
 
-
+  # get the form to make changes to the
+  # selected resource
 def edit
   set_ai
 end
 
+# saves the changes that where made in the edit form
 def update
   set_ai
   if @ai.update(ai_params)
@@ -59,6 +67,8 @@ def update
 end
 
 def destroy
+  # when you want to log someone out you
+# want to get rid of them in the session
   set_ai
   @ai.destroy
   redirect_to ais_path
